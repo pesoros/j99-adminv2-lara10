a7e025d305d7228f1870bb4095e4760f03642129
Bayu Yuhartono <bayuyuhartono@gmail.com>
2023-12-02 21:55:13 +0700

    putback sidebar minify

--- resources/views/layouts/shared/navbar.blade.php
+++ resources/views/layouts/shared/navbar.blade.php
@@ -1,6 +1,9 @@
 <!-- Left navbar links -->
 <ul class="navbar-nav">
   <li class="nav-item">
+    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+  </li>
+  <li class="nav-item">
     <a class="nav-link" data-widget="fullscreen" href="#" role="button">
       <i class="fas fa-expand-arrows-alt"></i>
     </a>
