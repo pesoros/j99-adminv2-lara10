fdc2614c06118eb7c8b38689ba1d0c5d93550e6a
Bayu Yuhartono <bayuyuhartono@gmail.com>
2023-12-08 01:45:17 +0700

    hide icon parent menu

--- resources/views/layouts/shared/menu.blade.php
+++ resources/views/layouts/shared/menu.blade.php
@@ -4,14 +4,12 @@
       @if (!isset($menu->child))
         <li class="nav-item {{ (request()->segment(1) ==  $menu->url) ? 'menu-open' : '' }}">
           <a href="{{ url($menu->url) }}" class="nav-link {{ (request()->segment(1) == $menu->url) ? 'active' : '' }}">
-            <i class="far fa-circle nav-icon"></i>
             <p>{{ $menu->title }}</p>
           </a>
         </li>
       @else
         <li class="nav-item {{ (request()->segment(1) == $menu->url) ? 'menu-open' : '' }}">
           <a href="#" class="nav-link  {{ (request()->segment(1) == $menu->url) ? 'active' : '' }}">
-            <i class="far fa-circle nav-icon"></i>
             <p>
               {{ $menu->title }}
               <i class="right fas fa-angle-left"></i>
@@ -26,7 +24,7 @@
             @endphp
               <li class="nav-item">
                 <a href="{{ url($child->url) }}" class="nav-link {{ $segmentCheck ? 'active' : '' }}">
-                  <i class="fas fa-circle nav-icon"></i>
+                  <i class="far fa-circle nav-icon"></i>
                   <p>{{ $child->title }}</p>
                 </a>
               </li>
