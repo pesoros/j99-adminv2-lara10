1a09dc0b39056853a79ecfceb12cad1c8d9c104c
Bayu Yuhartono <bayuyuhartono@gmail.com>
2024-03-15 13:44:09 +0700

    fix search in drop class

--- Modules/Masterdata/resources/views/class/add.blade.php
+++ Modules/Masterdata/resources/views/class/add.blade.php
@@ -50,7 +50,7 @@
           <label>Fasilitas</label>
           <select class="select2 select2-hidden-accessible" multiple="" name="facilities[]" data-placeholder="Pilih fasilitas" style="width: 100%;" data-select2-id="7" tabindex="-1" aria-hidden="true">
             @foreach ($facilities as $facility)
-                <option data-select2-id="{{ $facility->id }}" value="{{ $facility->id }}">
+                <option value="{{ $facility->id }}">
                     {{ $facility->name }}
                 </option>
             @endForeach
